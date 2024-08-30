96387ee72b95c7412f7ff9e7aae3b75244b99deb
Rica Duchateau <rduchateau@clintonhealthaccess.org>
2024-08-30 11:20:00 +0700

    Fix package checks

--- R Processing Scripts/process_alt.R
+++ R Processing Scripts/process_alt.R
@@ -2,7 +2,7 @@
 setwd("D:/Malaria/por-laos-2024")
 
 # Load necessary libraries
-required_packages <- c("exactextractr", "dplyr")
+required_packages <- c("exactextractr", "dplyr", "sf", "terra")
 installed_packages <- rownames(installed.packages())
 
 for (pkg in required_packages) {
@@ -13,6 +13,7 @@ for (pkg in required_packages) {
 
 library(exactextractr)
 library(dplyr)
+library(sf)
 library(terra)
 
 # Function to display a specific band of the raster in RStudio with a custom title
@@ -23,13 +24,13 @@ display_raster <- function(raster_file) {
 
 # Function to calculate zonal statistics and save to CSV
 calculate_zonal_statistics <- function(raster_file, shapefile, output_csv) {
-  try({
+  tryCatch({
     polygons <- st_read(shapefile)
     print(paste("Shapefile loaded successfully. Number of features:", nrow(polygons)))
     
-    r <- raster(raster_file)
-    if (st_crs(polygons)$proj4string != projection(r)) {
-      polygons <- st_transform(polygons, crs = projection(r))
+    r <- rast(raster_file)
+    if (st_crs(polygons)$proj4string != crs(r)) {
+      polygons <- st_transform(polygons, crs = crs(r))
       print("Shapefile reprojected to match the raster CRS.")
     }
     
@@ -50,7 +51,9 @@ calculate_zonal_statistics <- function(raster_file, shapefile, output_csv) {
     data <- read.csv(output_csv)
     View(data)
     
-  }, silent = FALSE)
+  }, error = function(e) {
+    message("An error occurred: ", e)
+  })
 }
 
 # Function to plot a shapefile in R
@@ -62,7 +65,7 @@ plot_shapefile <- function(shapefile, title) {
 
 # Set file paths
 download_file <- "D:/ERA5/SRTM_DEM_2000_Laos.tif"
-shapefile <- "D:/ERA5/hfca_only_public_072024.shp"
+shapefile <- "D:/ERA5/HFML for POR Voronoi.shp"
 output_csv <- "D:/ERA5/alt_zonal_statistics.csv"
 
 # Display DEM
@@ -71,6 +74,7 @@ display_raster(download_file)
 # Plot the shapefile
 plot_shapefile(shapefile, "Health Facility Catchment Areas")
 
+# Calculate zonal statistics
 calculate_zonal_statistics(download_file, shapefile, output_csv)
 
 print("Process completed.")
